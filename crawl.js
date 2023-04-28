772db0327b9947c23f50a9385159ac69421f0238
Sunil <NEYLHAM11@GMAIL.COM>
2023-04-28 22:54:10 +0530

    crawl page recursive working

--- crawl.js
+++ crawl.js
@@ -1,24 +1,47 @@
 const {JSDOM} = require('jsdom')
 
-async function crawlPage(currentURL) {
+async function crawlPage(baseURL,currentURL, pages) {
+
+    const baseURLObj = new URL (baseURL)
+    const currentURLObj = new URL (currentURL)
+    if(baseURLObj.hostname !== currentURLObj.hostname) {
+        return pages
+    }
+
+    const normalizedCurrentURL = normalizeURL(currentURL)
+    if(pages[normalizedCurrentURL] > 0) {
+        pages[normalizedCurrentURL]++
+        return pages
+    }
+
+    pages[normalizedCurrentURL] = 1
     console.log(`actively crawling: ${currentURL} `)
+
     try {
         const resp = await fetch(currentURL)
         if(resp.status > 399) {
             console.log(`error in fetch status code ${resp.status} on page ${currentURL}`)
-            return
+            return pages
         }
 
         const contentType = resp.headers.get('content-type')
         if(!contentType.includes('text/html')) {
             console.log(`no html response, content type ${contentType} on page ${currentURL}`)
-            return
+            return pages
         }
 
-        console.log(await resp.text()) 
+        const htmlbody = await resp.text()
+
+       const nextURLs = getURLsFromHTML(htmlbody, baseURL)
+
+       for(const nextURL of nextURLs) {
+        pages = await crawlPage(baseURL, nextURL, pages)
+       }
+
     } catch (error) {
         console.log(`Error in page: ${error.message}, on page: ${currentURL}`)
     }
+    return pages
 }
 
 function getURLsFromHTML (htmlBody, baseURL) {
